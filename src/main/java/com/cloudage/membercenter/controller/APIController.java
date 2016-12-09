97479887cd499f272a49baa6fde4b2cafb8f030d
Administrator <Administrator@7A304-0001>
2016-12-09 10:55:24 +0800

    照片上传及访问

--- src/main/java/com/cloudage/membercenter/controller/APIController.java
+++ src/main/java/com/cloudage/membercenter/controller/APIController.java
@@ -1,11 +1,17 @@
 package com.cloudage.membercenter.controller;
 
+import java.io.File;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.cloudage.membercenter.entity.User;
 import com.cloudage.membercenter.service.IUserService;
@@ -28,13 +34,25 @@ public class APIController {
 			@RequestParam String account,
 			@RequestParam String passwordHash,
 			@RequestParam String email,
-			@RequestParam String name){
+			@RequestParam String name,
+			MultipartFile avatar,
+			HttpServletRequest request){
 		
 		User user = new User();
 		user.setAccount(account);
 		user.setPasswordHash(passwordHash);
 		user.setEmail(email);
 		user.setName(name);
+		
+		if(avatar!=null){
+			try{
+				String realPath = request.getSession().getServletContext().getRealPath("/WEB-INF/upload");
+				FileUtils.copyInputStreamToFile(avatar.getInputStream(), new File(realPath,account+".png"));
+				user.setAvatar("upload/"+account+".png");
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 		
 		return userService.save(user);
 	}
